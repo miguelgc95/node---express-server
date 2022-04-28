b1f8752b6249271190a20fae9b8df5cda35521e4
miguelgc95 <miguelgc95@gmail.com>
2022-04-28 17:14:42 +0200

    adapt userController to work with new user model

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,20 +1,20 @@
-import UserModel from '../models/UserModel.js';
+import UserModel from '../models/UserModel.js'
 
 async function getUser(req, res) {
-	const userMail = 'mike@test.com';
+	const mail = 'mike@test.com'
 	// const userMail = req.user.mail;
 	try {
-		// const response = await UserModel.findOne({ userMail }).lean().exec();
-		const response = 'q dise maik';
-		console.log(response);
+		const response = await UserModel.findOne({ mail }).lean().exec()
+		// const response = 'q dise maik';
+		console.log(response)
 
-		if (response) return res.status(200).send(response);
-		return res.status(400).send('error');
+		if (response) return res.status(200).send(response)
+		return res.status(400).send('error')
 	} catch (error) {
-		console.log(error);
+		console.log(error)
 		// next(error)??
-		res.json(error);
+		res.json(error)
 	}
 }
 
-export { getUser };
+export { getUser }
